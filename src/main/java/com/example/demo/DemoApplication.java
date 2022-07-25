2eaa91e9143ec12ee12da4f80a391d000c44294c
Himansu-git <109942115+Himansu-git@users.noreply.github.com>
2022-07-25 20:03:08 +0530

    Add files via upload
    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @GetMapping("/message")
+    public String message() {
+
+        return "hello ! welcome to cloud";
+
+    }
+
+    @GetMapping("/welcome/{name}")
+    public String welcome(@PathVariable("name") String name) {
+
+            return "Welcome -" + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
